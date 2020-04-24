78cd075d73cd092316b7d041aaf61bbd36ca51bc
Fernando Navarro <49495922+NavarroFer@users.noreply.github.com>
2020-04-24 02:56:18 -0300

    Acomodado y actualizado

--- Src/PowerUp.gd
+++ Src/PowerUp.gd
@@ -4,37 +4,36 @@ var Vacuna = preload("res://Sprites/PowerUps/Vacuna.png")
 var Barbijo = preload("res://Sprites/PowerUps/Barbijo.png")
 var Jabon = preload("res://Sprites/PowerUps/Jabon.png")
 var Botas = preload("res://Sprites/PowerUps/Bota.png")
-var time
 
 export (int) var Tipo
-signal item_picked
 
 const VACUNA = 1
-const TIME_VACUNA = 5
 const BARBIJO = 2
-const TIME_BARBIJO = 5
 const JABON = 3
-const TIME_JABON = 10
 const BOTAS = 4
-const TIME_BOTAS = 10
 
 func _ready():	
 	match Tipo:
 		VACUNA:
 			$Sprite.texture = Vacuna
-			time = TIME_VACUNA
 		BARBIJO:
 			$Sprite.texture = Barbijo
-			time = TIME_BARBIJO
 		JABON:
 			$Sprite.texture = Jabon
-			time = TIME_JABON
 		BOTAS:
 			$Sprite.texture = Botas
-			time = TIME_BOTAS
 	$AnimationPlayer.play("Idle")
 
-func _on_PowerUp_body_entered(body):	
+func _on_PowerUpVEL_body_entered(body):
 	if "Player" in body.name:
-		body.picked_power_up(Tipo,time)
+		match Tipo:
+			VACUNA:
+				body.picked_power_up_invulneravility()
+			BARBIJO:
+				body.picked_power_up_armor()
+			JABON:
+				body.picked_power_up_damage()
+			BOTAS:
+				body.picked_power_up_velocity()
+		
 		queue_free()
