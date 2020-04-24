1843f1323fdf014230061f017b8e152cf8ee6e14
Fernando Navarro <49495922+NavarroFer@users.noreply.github.com>
2020-04-24 02:29:37 -0300

    Add files via upload

---
+++ Src/ArmorBar.gd
@@ -0,0 +1,6 @@
+extends ProgressBar
+
+
+func _on_Player_armor_change(armor):
+	set_value(armor)
+	
---
+++ Src/HealthBar.gd
@@ -0,0 +1,5 @@
+extends ProgressBar
+
+func _on_Player_hp_changed(new_amount):
+	set_value(new_amount)	
+
