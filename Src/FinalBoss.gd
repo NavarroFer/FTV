f56bf83e3327f31d36aa3e6389bcfc3ae52deac1
Fernando Navarro <49495922+NavarroFer@users.noreply.github.com>
2020-04-26 22:53:45 -0300

    Animacion idle final boss

--- Src/FinalBoss.gd
+++ Src/FinalBoss.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
 var vel = Vector2()
-var direction = 1
+var direction_x = 1
+var direction_y = 1
 var is_dead = false
-var speed
+var speed_x
+var speed_y
 
 const FLOOR = Vector2(0,-1)
 const MAX_VELX = 200
@@ -11,13 +13,16 @@ const MAX_VELY = 200
 const DAMAGE_COLLISION = 10
 
 func _ready():
+	$AnimationPlayer.play("Idle")
 	pass
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
-	speed = MAX_VELX
+	speed_x = MAX_VELX
+	speed_y = MAX_VELY
 	if !is_dead:
-		vel.x = speed * direction
+		vel.x = speed_x * direction_x
+		vel.y = speed_y * direction_y
 		#if direction == 1:
 		#	$AnimatedSprite.flip_h = false
 		#else:
@@ -25,7 +30,11 @@ func _physics_process(delta):
 		#$AnimatedSprite.play("Walk")
 	vel = move_and_slide(vel,FLOOR)
 	if is_on_wall():
-			direction *= -1
+		direction_x *= -1
+	if is_on_ceiling():
+		direction_y *= -1
+	if is_on_floor():
+		direction_y *= -1
 	if get_slide_count() > 0:
 			for i in range(get_slide_count()):
 				if "Player" in get_slide_collision(i).collider.name:							
