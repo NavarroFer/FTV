42786aabd6739c9190333f2c7eebc10b5d4f8eaf
Fernando Navarro <49495922+NavarroFer@users.noreply.github.com>
2020-06-01 03:28:38 -0300

    Seleccion de skins, nuevo juego, continuar juego
    Icono cambiado

---
+++ Src/Game.gd
@@ -0,0 +1,42 @@
+extends Node
+
+var nivel_act = 0
+var skin = 0
+var in_game = false
+
+func _ready():
+	_load()
+	
+func _save():
+	var date = {
+		"nivel":nivel_act,
+		"skin":skin,
+		"in_game":in_game
+	}
+	print("NIVEL ACTUAL "+str(nivel_act))
+	Saves.save_game(date)
+	
+func _load():
+	var date = {}
+	date = Saves.load_game()
+	nivel_act = int(date["nivel"])
+	skin = int(date["skin"])
+	in_game = bool(date["in_game"])
+	return date
+	
+func setSkin(s):
+	skin = s
+	print(skin)
+	_save()
+	
+func getNivel():
+	return nivel_act
+
+func setNivel(n):
+	nivel_act = n
+
+func hayPartidaIniciada():
+	return in_game == true
+	
+func setInGame(boolean):
+	in_game = boolean
---
+++ Src/SkinSelector.gd
@@ -0,0 +1,40 @@
+extends Control
+
+
+const CANT_SKINS = 4
+const MIN_VALUE_SKINS = 1
+var skins = [preload("res://Sprites/Player/SSA.png"),preload("res://Sprites/Player/SSA_Blue.png"),preload("res://Sprites/Player/SSA_Green.png"),preload("res://Sprites/Player/SSA_Pink.png")]
+var niveles = ["res://Scenes/World.tscn"
+,"res://Scenes/World2.tscn"
+,"res://Scenes/World3.tscn"
+,"res://Scenes/WorldFinal.tscn"]
+
+var selected = MIN_VALUE_SKINS
+
+func _ready():
+	$AnimationPlayer.play("walk")
+
+func change_skin_r():
+	if selected < CANT_SKINS:
+		selected += 1
+	set_skin()
+	
+func change_skin_l():
+	if selected > MIN_VALUE_SKINS:
+		selected -= 1
+	set_skin()
+
+func set_skin():
+	$Control/Sprite.texture = skins[selected-1]
+
+func _on_Button_pressed():
+	#Escribir en un archivo el skin que eligio
+	#para usar en todos los niveles
+	Game.setSkin(selected)	
+	get_tree().change_scene(niveles[int(Game.getNivel())])
+
+func _on_Der_pressed():
+	change_skin_r()
+
+func _on_Izq_pressed():
+	change_skin_l()
